448c8205f794e56ae8dcbdbee7ff735c152b6df6
BertZZ <bertie.wooles@gmail.com>
2020-09-29 14:52:35 +0100

    Fixes rails version and allows internal app tests to run

--- hyrax-hirmeos.gemspec
+++ hyrax-hirmeos.gemspec
@@ -25,8 +25,7 @@ Gem::Specification.new do |spec|
 
   spec.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
 
-  spec.add_dependency "rails", "~> 6.0.3", ">= 6.0.3.3"
+  spec.add_dependency "rails", "~> 5.2.4", ">= 5.2.4.3"
 
-  spec.add_development_dependency "sqlite3"
   spec.add_development_dependency "rspec-rails"
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,7 @@
 require 'spec_helper'
 require File.expand_path('internal_test_hyrax/spec/rails_helper.rb', __dir__)
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
+require File.expand_path('internal_test_hyrax/config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
