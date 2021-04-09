24accffbfdebb3ccf3f6cf5be03b97a583497cf9
BertZZ <bertie.wooles@gmail.com>
2021-04-09 13:16:59 +0100

    Removes files code from work factory

--- lib/hyrax/hirmeos/work_factory.rb
+++ lib/hyrax/hirmeos/work_factory.rb
@@ -8,20 +8,9 @@ class Hyrax::Hirmeos::WorkFactory
     work.uri = [{ uri: resource_url(resource), canonical: true },
                 { uri: "urn:uuid:#{resource.id}" }]
     work.type = "repository-work"
-    file_urls = add_file_urls(resource)
-    work.uri << file_urls if file_urls.present?
-    work.uri.flatten!
-    work
   end
 
   def self.resource_url(work)
     Rails.application.routes.url_helpers.polymorphic_url(work)
-  end
-
-  def self.add_file_urls(work)
-    files = work.file_sets
-    return if files.blank?
-    links = files.map { |file| Hyrax::Engine.routes.url_helpers.download_url(id: file) }
-    links.map { |link| { uri: link } }
   end
 end
