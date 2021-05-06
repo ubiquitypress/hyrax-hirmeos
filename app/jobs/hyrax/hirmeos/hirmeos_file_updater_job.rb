959045d8a20a5a922c056690bb52c41e2ecb780f
BertZZ <bertie.wooles@gmail.com>
2021-05-06 16:34:35 +0100

    Fixes missing ID call

--- app/jobs/hyrax/hirmeos/hirmeos_file_updater_job.rb
+++ app/jobs/hyrax/hirmeos/hirmeos_file_updater_job.rb
@@ -2,7 +2,7 @@
 module Hyrax
   module Hirmeos
     class HirmeosFileUpdaterJob < ApplicationJob
-      def perform(resource)
+      def perform(resource_id)
         resource = ActiveFedora::Base.find(resource_id)
         Hyrax::Hirmeos::MetricsTracker.new.submit_file_to_hirmeos(resource)
       end
