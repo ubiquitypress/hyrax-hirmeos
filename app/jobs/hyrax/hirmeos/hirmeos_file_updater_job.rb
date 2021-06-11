2302297c9e08531fed6e0be8e94c599e38a34bb6
Paul Danelli <prdanelli@gmail.com>
2021-06-11 10:35:04 +0100

    Prevent failed jobs from clogging up redis if there is no availble connection

--- app/jobs/hyrax/hirmeos/hirmeos_file_updater_job.rb
+++ app/jobs/hyrax/hirmeos/hirmeos_file_updater_job.rb
@@ -2,6 +2,9 @@
 module Hyrax
   module Hirmeos
     class HirmeosFileUpdaterJob < ApplicationJob
+      # If there is no available connection, drop the job
+      discard_on Faraday::ConnectionFailed
+
       def perform(resource_id)
         resource = ActiveFedora::Base.find(resource_id)
         Hyrax::Hirmeos::MetricsTracker.new.submit_file_to_hirmeos(resource)
